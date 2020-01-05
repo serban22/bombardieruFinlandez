a49b44b0e3a1701c84c027dde6ce277088d61038
serban221297@gmail.com <serban221297@gmail.com>
2020-01-05 15:18:21 +0200

    Week 2 done

--- src/main/java/Week2/ex42_HangmanGame.java
+++ src/main/java/Week2/ex42_HangmanGame.java
@@ -1,39 +1,41 @@
 package Week2;
 
+import hangman.Hangman;
 import java.util.Scanner;
 
+
 public class ex42_HangmanGame {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Hangman hangman = new Hangman();
 
-    public boolean GameOn() {
-        return true;
-    }
+        System.out.println("************");
+        System.out.println("* Hangman *");
+        System.out.println("************");
+        System.out.println("");
+        printMenu();
+        System.out.println("");
 
-    public void printStatus() {
-        System.out.println("You have not made any guesses yet.");
-        System.out.println("Unused letters: abcdefghijklmnoprstuvwxyz");
-    }
-
-    public void printWord() {
-        StringBuilder unknownWord = new StringBuilder("?????????");
-        System.out.println("Word to be guessed: " + unknownWord);
-    }
-
-    public void printMan() {
-        System.out.println(" __ __");
-        System.out.println(" |");
-        System.out.println(" |");
-        System.out.println(" |");
-        System.out.println(" |");
-        System.out.println("/|");
-    }
-
-    public void guess(String letter) {
-        String secretWord = "christmas";
-        if (secretWord.contains(letter)) {
-            System.out.println("The letter " + letter + " was found in the word.");
-        } else {
-            System.out.println("The letter " + letter + " is not in this word.");
+        while (hangman.gameOn()) {
+            System.out.print("Type the desired command: ");
+            String command = input.nextLine();
+            if (command.equalsIgnoreCase("quit")) {
+                System.out.println("Thank you for playing!");
+                break;
+            } else if (command.equalsIgnoreCase("status")) {
+                hangman.printStatus();
+            } else if (command.length() == 1) {
+                hangman.guess(command);
+            } else if (command.isEmpty()) {
+                System.out.println("You didn't type anything!");
+                printMenu();
+            } else {
+                System.out.println("I found a letter!");
+            }
+            hangman.printMan();
+            hangman.printWord();
         }
+        System.out.println("Thank you for playing!");
     }
 
     public static void printMenu() {
@@ -42,41 +44,5 @@ public class ex42_HangmanGame {
         System.out.println("status  - prints the game status");
         System.out.println("a single letter uses the letter as a guess");
         System.out.println("an empty line prints this menu");
-    }
-}
-
-class HangmanGameRunner {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        ex42_HangmanGame hangmanGame = new ex42_HangmanGame();
-
-        System.out.println("************");
-        System.out.println("* Hangman *");
-        System.out.println("************");
-        System.out.println("");
-        ex42_HangmanGame.printMenu();
-        System.out.println("");
-
-        while (hangmanGame.GameOn()) {
-            System.out.print("Type the desired command: ");
-            String command = input.nextLine();
-
-            if (command.equalsIgnoreCase("quit")) {
-                System.out.println("Thank you for playing!");
-                break;
-            } else if (command.equalsIgnoreCase("status")) {
-                hangmanGame.printStatus();
-            } else if (command.length() == 1) {
-                hangmanGame.guess(command);
-            } else if (command.isEmpty()) {
-                System.out.println("Empty string!");
-                ex42_HangmanGame.printMenu();
-            } else {
-                System.out.println("Found a letter!");
-            }
-            hangmanGame.printMan();
-            hangmanGame.printWord();
-        }
-        System.out.println("Thank you for playing!");
     }
 }
