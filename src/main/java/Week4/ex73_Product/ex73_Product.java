3560b2c1e189540e2a04ab483e27e5e8abc5e2fa
serban221297@gmail.com <serban221297@gmail.com>
2020-01-10 18:14:32 +0200

    Exercise 73 from week 4 (as a package).

--- src/main/java/Week4/ex73_Product/ex73_Product.java
+++ src/main/java/Week4/ex73_Product/ex73_Product.java
@@ -6,10 +6,12 @@ public class ex73_Product {
         Product apple = new Product(6, 10, "Apple");
         Product orange = new Product(8, 7, "Orange");
         Product mango = new Product(10, 6, "Mango");
+        Product passionFruit = new Product(12, 10, "Passion fruit");
 
         banana.printProduct();
         apple.printProduct();
         orange.printProduct();
         mango.printProduct();
+        passionFruit.printProduct();
     }
 }
